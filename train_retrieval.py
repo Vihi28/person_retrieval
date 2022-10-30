d8bf00afed5ab4f98b7fd1aedc888ede2a96d33d
Vihi28 <108642362+Vihi28@users.noreply.github.com>
2022-10-30 17:17:02 +0530

    Update train_retrieval.py

--- train_retrieval.py
+++ train_retrieval.py
@@ -273,8 +273,8 @@ def main(args, config):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--config', default='./configs/retrieval_flickr.yaml')
-    parser.add_argument('--output_dir', default='output/Retrieval_flickr')
+    parser.add_argument('--config', default='/content/person_retrieval/configs/retrieval_flickr.yaml')
+    parser.add_argument('--output_dir', default='/content/person_retrieval/output/Retrieval_flickr')
     parser.add_argument('--evaluate', action='store_true')
     parser.add_argument('--device', default='cuda')
     parser.add_argument('--seed', default=42, type=int)
